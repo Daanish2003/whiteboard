e48e9f8322fa6885af7c1a7e8ad808b23250504f
Daanish Azim Sheikh <redchillies6786@gmail.com>
2024-03-06 12:42:47 +0530

    created line and rectangle elements

--- src/components/drawingTools.tsx
+++ src/components/drawingTools.tsx
@@ -1,31 +1,106 @@
 "use client"
-import { useLayoutEffect, useRef } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import rough from "roughjs"
 
+type ElementType = {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  roughElement: any;
+}
+
 export default function DrawingTools() {
+  const [elements, setElements] = useState<ElementType[]>([]);
+  const [drawing, setDrawing] = useState(false);
+  const [elementType, setElementType] = useState<
+    "line" | "rectangle" | "circle"
+  >("line");
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const gen = rough.generator();
+
+  const createElement = (x1: number, y1: number, x2: number, y2: number): ElementType => {
+    const roughElement = elementType === "line"
+      ? gen.line(x1, y1, x2, y2)
+      : gen.rectangle(x1, y1, x2 - x1, y2 - y1);
+    return { x1, y1, x2, y2, roughElement };
+  }
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    if (window) { // Check if window is defined
+      canvasRef.current.width = window.innerWidth;
+      canvasRef.current.height = window.innerHeight;
+    }
+  }, []);
 
   useLayoutEffect(() => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     const rc = rough.canvas(canvas);
-    const rectangle = gen.rectangle(10, 10, 200, 200);
-    const line = gen.line(80, 130, 300, 100);
-    rc.draw(rectangle);
-    rc.draw(line)
-  })
+    
+    elements.forEach(({ roughElement }) => {
+      rc.draw(roughElement);
+    });
+  }, [elements])
   
+  const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    setDrawing(true);
+    const { clientX, clientY } = event;
+    const element = createElement(clientX, clientY, clientX, clientY);
+    setElements((prevElements) => [...prevElements, element]);
+  };
+  
+  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!drawing) return;
+    const index = elements.length - 1;
+    const { clientX, clientY } = event;
+    const {x1, y1} = elements[index];
+    const updatedElement = createElement(x1, y1, clientX, clientY);
+
+    const elementCopy = [...elements];
+    elementCopy[index] = updatedElement;
+    setElements(elementCopy);
+  };
+
+  const handleMouseUp = () => {
+    setDrawing(false);
+  }
   
 
-  return (
-    <div className="flex items-center justify-center h-screen w-screen">
-    <canvas
-      id="canvas"
-      width={750}
-      height={750}
-    >
-      canvas
-    </canvas>
+   return (
+    <div>
+      <div style={{ position: "fixed" }}>
+
+        <input
+          type="radio"
+          name="line"
+          id="line"
+          checked={elementType === "line"}
+          onChange={() => setElementType("line")}
+        />
+        <label htmlFor="line">line</label>
+
+        <input
+          type="radio"
+          name="rectangle"
+          id="rectangle"
+          checked={elementType === "rectangle"}
+          onChange={() => setElementType("rectangle")}
+        />
+
+        <label htmlFor="rectangle">rectangle</label>
+      </div>
+      <canvas
+        ref={canvasRef}
+        id="canvas"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+        Canvas{" "}
+      </canvas>
     </div>
-  )
+  );
 } 
